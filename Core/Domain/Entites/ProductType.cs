a16937dd98e9a9877cdbe472dc287dbce655ed2c
esraanebroo <ea1543347@gmail.com>
2025-03-22 21:44:08 +0200

    DbContext-Configration

---
+++ Core/Domain/Entites/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entites
+{
+    public class ProductType:BaseEntity<int>
+    {
+        //Id
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Presistance/Global/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Domain.Entites;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using System.Reflection;
+
