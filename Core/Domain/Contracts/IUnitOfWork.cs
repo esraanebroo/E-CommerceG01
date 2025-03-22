d9bd30980e8e2d3d3b3cf229689e5326594a466a
esraanebroo <ea1543347@gmail.com>
2025-03-23 00:14:08 +0200

    UnitOfWork

---
+++ Core/Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+        IGenaricRepository<TEntity, Tkey> GenaricRepository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>;
+    }
+}
