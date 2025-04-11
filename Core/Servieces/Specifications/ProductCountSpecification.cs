062152cd9d14f186df9d6d37caef25d46f8a049e
esraanebroo <ea1543347@gmail.com>
2025-04-12 02:30:41 +0300

    Search

--- Core/Servieces/Specifications/ProductCountSpecification.cs
+++ Core/Servieces/Specifications/ProductCountSpecification.cs
@@ -11,7 +11,9 @@ namespace Servieces.Specifications
         public ProductCountSpecification(ProductParametersSpecifications specifications)
             : base(product =>
             (!specifications.BrandId.HasValue || product.BrandId == specifications.BrandId.Value) &&
-            (!specifications.TypeId.HasValue || product.TypeId == specifications.TypeId.Value)
+            (!specifications.TypeId.HasValue || product.TypeId == specifications.TypeId.Value)&&
+            (string.IsNullOrWhiteSpace(specifications.Search) || product.Name.ToLower().Contains(specifications.Search.ToLower().Trim()))
+
             )
         {
           
